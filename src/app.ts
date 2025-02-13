879ddb89a43b3265058492d5c41bcd27804dc13e
mehedimrm22 <mehedi.mrm@gmail.com>
2025-02-13 14:26:28 +0200

    api

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const form = document.querySelector("form");
 const addressInput = document.getElementById("address")! as HTMLInputElement;
 
-const GOOGLE_API_KEY = "AIzaSyAuyPWb0vgdZ3j5PZ6ptox5pFoEUMoYMao";
+const GOOGLE_API_KEY = "APIß";
 
 declare let google: any;
 
